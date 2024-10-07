a0ff5b31400c56ff7b364f3c43e41f832e660bf1
Tim Stoldt <timstoldt@Tims-MBP.lan>
2024-10-07 13:11:43 -0700

    added bertha the tritoon

--- src/pages/Fleet.js
+++ src/pages/Fleet.js
@@ -25,11 +25,20 @@ const Fleet = () => {
         buttonUrl={process.env.REACT_APP_BOOKING_URL}
       />
       <LineItem
+        title={"26ft Tahoe Tritoon"}
+        description={
+          "Perfect for larger groups or families looking for a fun day on the water. This tritoon comfortably seats up to 15 passengers and offers a stable and smooth ride, ideal for both relaxation and adventure. Powered by a 175hp Yamaha VMAX outboard engine, it delivers plenty of power for cruising. The boat also includes a Bimini top for shade at the stern. With a 35 gallon fuel capacity, you'll have everything you need for an unforgettable day on the lake.</a>"
+        }
+        photo={"/bertha.jpeg"}
+        buttonTitle={"Book Online Now!"}
+        buttonUrl={process.env.REACT_APP_BOOKING_URL}
+      />
+      <LineItem
         title={"24ft Avalon Pontoon"}
         description={
-          "Our rental fleet features a spacious 24ft Avalon pontoon, perfect for a relaxing day on the water. This pontoon comfortably seats up to 13 passengers (up to 1850 lbs) and comes equipped with a Bimini top to provide shade at the stern. Enjoy the convenience of a 5-inch Garmin Chartplotter/Fishfinder, making navigation and fishing easier. Powered by a quiet and efficient 90hp 2023 Yamaha 4-Stroke EFI outboard engine, this pontoon also includes a 25-gallon fuel tank, ensuring you have plenty of fuel for your adventures.</a>"
+          "Perfect for a relaxing day on the water. This pontoon comfortably seats up to 13 passengers (up to 1850 lbs) and comes equipped with a Bimini top to provide shade at the stern. Enjoy the convenience of a 5-inch Garmin Chartplotter/Fishfinder, making navigation and fishing easier. Powered by a quiet and efficient 90hp 2023 Yamaha 4-Stroke EFI outboard engine, this pontoon also includes a 25-gallon fuel tank, ensuring you have plenty of fuel for your adventures.</a>"
         }
-        photo={"/toonseats.jpg"}
+        photo={"/toon.jpg"}
         buttonTitle={"Book Online Now!"}
         buttonUrl={process.env.REACT_APP_BOOKING_URL}
       />
